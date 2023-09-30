ff415e628fad1aef85cd51fe233c54686c53d4f1
JoacoCinnamon <joacopontello2002@gmail.com>
2023-09-30 04:17:36 -0300

    Home

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -10,7 +10,20 @@ export default function Home({
                 <section className="h-auto">
                     <div className="px-10 py-24 mx-auto max-w-7xl">
                         <div className="w-full mx-auto text-left md:text-center">
-                            <h1 className="mb-6 text-5xl font-extrabold leading-none max-w-5xl mx-auto tracking-normal  sm:text-6xl md:text-6xl lg:text-7xl md:tracking-tight text-center">
+                            <div className="relative isolate">
+                                <div
+                                    aria-hidden='true'
+                                    className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+                                    <div
+                                        style={{
+                                            clipPath:
+                                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                                        }}
+                                        className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-10 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+                                    />
+                                </div>
+                            </div>
+                            <h1 className="text-balance mb-6 text-5xl font-extrabold leading-none max-w-5xl mx-auto tracking-normal  sm:text-6xl md:text-6xl lg:text-7xl md:tracking-tight text-center">
                                 Vetiquetas: <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 lg:inline">
                                     bordando identidades</span>
                                 <span className="lg:block hidden">simplificando pedidos.</span>
