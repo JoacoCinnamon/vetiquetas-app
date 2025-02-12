b7ce5d663a97284c29267339f9bf204da5c116a3
JoacoCinnamon <joacopontello2002@gmail.com>
2025-02-12 16:18:50 -0300

    Update PDF generation to use Laravel SnappyPDF (instead of DomPDF) and add WKHTML_PDF_BINARY to .env.example

--- config/app.php
+++ config/app.php
@@ -187,7 +187,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
     ])->toArray(),
 
 ];
